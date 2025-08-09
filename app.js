c97febb51c17a21b6474675ff8a7675b4d6de90f
robertomoreirasantos-cloud <robertomoreirasantos@pos.fafire.br>
2025-08-09 17:49:45 +0000

    atualização arquivo teste.txt e app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
     res.send('Hello Geeks!');
+    res.send('ola')
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
